a4d77210c64b1b95de813d634289760912deb934
Diencoder <156517566+Diencoder@users.noreply.github.com>
2024-10-31 12:43:49 +0700

    Add files via upload

---
+++ KoiDeliveryOrderingSystem.Respositories/Data/Feedback.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KoiDeliveryOrderingSystem.WebApplication.Data;
+
+public partial class Feedback
+{
+    public int FeedbackId { get; set; }
+
+    public int UserId { get; set; }
+
+    public int ServiceId { get; set; }
+
+    public int? Rating { get; set; }
+
+    public string? Comment { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual Service Service { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
---
+++ KoiDeliveryOrderingSystem.Respositories/Data/Invoice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KoiDeliveryOrderingSystem.WebApplication.Data;
+
+public partial class Invoice
+{
+    public int InvoiceId { get; set; }
+
+    public int OrderId { get; set; }
+
+    public int UserId { get; set; }
+
+    public string Status { get; set; } = null!;
+
+    public decimal? AmountDue { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+
+    public virtual Order Order { get; set; } = null!;
+
+    public virtual User User { get; set; } = null!;
+}
